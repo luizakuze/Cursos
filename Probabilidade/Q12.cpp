86d7a2512d6af97218d574292cf9e76402d87630
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-09-19 13:12:33 -0300

    Update Q12.cpp

--- Probabilidade/Q12.cpp
+++ Probabilidade/Q12.cpp
@@ -1,5 +1,3 @@
-/// TERMINAR!!!
-
 /*  Antes de serem carregados em um caminhão de distribuição, os pacotes estão sujeitos a testes independentes,
     para garantir que o motorista do caminhão possa segurá-los com segurança.
     O peso do pacote não deve exceder 30 kg e a soma das três dimensões deve ser inferior a 1m.
@@ -9,42 +7,48 @@
 /* ---> t1 = exceder o limite de peso
  * ---> t2 = exceder o limite de dimensão */
 
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-// using namespace std;
+using namespace std;
 
-// int main() {
-//     srand(time(NULL)); // inicializa o gerador de números aleatórios
+int main() {
+    srand(time(NULL)); // inicializa o gerador de números aleatórios
 
-//     unsigned n = 1000000; // número de simulações
+    unsigned n = 1000000; // número de simulações
 
-//     // probabilidade dos testes
-//     const int t1 = 0.05;
-//     const int t1c = 0.95; // 1 - t1
-//     const int t2 = 0.02;
-//     const int t2c = 0.98; // 1 - t2
+    // probabilidade dos testes
+    const float t1 = 0.05;
+    const float t2 = 0.02;
 
-//     float prob_t1 = 0;
-//     float prob_t2 = 0;
+    float prob_t1 = 0;
+    float prob_t2 = 0;
+    float prob_t2t1 = 0;
 
-//     // executa as simulações
-//     for (unsigned i = 0; i < n; i++) {
-//         int P_escolhido = rand() % 4; // escolhe uma porta entre 0, 1, 2, 3
+    // executa as simulações
+    for (unsigned i = 0; i < n; i++) {
+        float peso_random = rand ()% 2;  // 0 e 1 para peso
+        float dimensao_random = rand ()% 2;  // 0 e 1 para dimensão
 
-//     }
+        // se o pacote excede o limite de peso
+        if (peso_random <= t1) {
+            prob_t1 += 1.0;
 
-//     // dividindo os casos favoráveis pelo número total de casos
-//     prob_p1 /= n;
-//     prob_p2 /= n;
-//     prob_p3 /= n;
-//     prob_p4 /= n;
+            // se exceder o limite de dimensão também
+            // já é a probabilidade da interseção de t1 e t2
+            if (dimensao_random <= t2)
+                prob_t2t1 += 1.0;
+        }
+    }
 
-//     // probabilidade de t1 falhar e r
-//     float prob_total = prob_p1 + prob_p2 + prob_p3 + prob_p4;
+    prob_t1 /= n;
+    prob_t2 /= n;
+    prob_t2t1 /= prob_t1; // prob t2 dado que t1
 
-//     std::cout << "Probabilidade de sucesso total: " << prob_total << "\n";
+    std::cout << "P(A): " << prob_t1 << "\n";
+    std::cout << "P(B): " << prob_t2 << "\n";
+    std::cout << "P(B|A): " << prob_t2t1 << "\n";
 
-//     return 0;
-// }
+    return 0;
+}
