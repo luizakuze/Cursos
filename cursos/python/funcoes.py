d2a1201c7ceb43b616dbcd72f25ebaa51a044714
luizakuze <luizakuze08@gmail.com>
2024-01-22 07:35:54 -0300

    funcoes

---
+++ cursos/python/funcoes.py
@@ -0,0 +1,15 @@
+# funções
+# deixar dios espaços após término de funções!
+
+def boas_vindas():
+    print('Olá')
+    print('Tudo bem?')
+
+
+def somar_dois_numeros(numero1, numero2):
+    resultado = numero1 + numero2
+    print(resultado)
+
+
+somar_dois_numeros(1, 2)
+boas_vindas()
