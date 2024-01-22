649f24771bab95d11e426099f37c4a0e693f947c
luizakuze <luizakuze08@gmail.com>
2024-01-22 06:47:09 -0300

    operadores

---
+++ cursos/python/operadores.py
@@ -0,0 +1,13 @@
+# valor exponencial
+calculo = 2 ** 3
+print(calculo)
+
+# operador de comparação
+operador = 10 <= 20
+print(operador)
+
+# oeprador de atribuição
+x = 2
+x = x + 5
+x += 5
+print(x)
