e75dfca119f9daea1a9e92aa1360502416395595
luizakuze <luizakuze08@gmail.com>
2024-01-22 07:39:32 -0300

    funções

--- cursos/python/funcoes.py
+++ cursos/python/funcoes.py
@@ -13,3 +13,23 @@ def somar_dois_numeros(numero1, numero2):
 
 somar_dois_numeros(1, 2)
 boas_vindas()
+
+# non-default e default
+def boas_vindas2(quantidade, nome='Luiza'): # quantidade = non-default; nome = default, deve ser e
+    print(f'Olá {nome}')
+    print(f' temos {str(quantidade)} de produtos em estoque')
+
+
+def cliente1(nome):
+    print(f'Olá{nome}')
+
+
+cliente1('Luiza')
+
+# função com retorno
+def cliente2(nome):
+    return f'Olá{nome}'
+
+
+nome_retornado = cliente2('Luiza')
+print(nome_retornado)
