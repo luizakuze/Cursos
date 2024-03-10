1f6d71331f27d2b65e2dd5acce215b2941f849ef
luizakuze <luizakuze08@gmail.com>
2024-03-10 14:54:00 -0300

    falta adicionar arquivo do próprio matlab!

--- cálculo-numérico/métodos/raízes-de-função/bissecao.m
+++ cálculo-numérico/métodos/raízes-de-função/bissecao.m
@@ -1,1 +1,11 @@
 % Método da Bisseção
+%%% Subdivisões sucessivas para intervalo entre A e B, mantendo sempre um positivo e um negativo.
+
+%% definindo uma função e valores em que a função dê um positivio e um negativo
+f = @(x)sin(x);
+a = 2;
+b = 4;
+fa = f(a);
+fb = f(b);
+disp("Valores: %d %d", fa fb);
+
